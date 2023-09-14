cb8c9b558a834b8f26ba746c96e9ef99e370da67
TomHatesStuff <tlukemarshall1@gmail.com>
2023-09-13 18:24:10 -0600

    Fixed original mistake

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+                Console.Write ("what was your grade percentage for this class? ");
+        string number = Console.ReadLine ();
+        int percent = int.Parse (number);
+
+        if (percent >= 90)
+        {
+            Console.WriteLine("Your grade is an A ");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("Your grade is an B ");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("Your grade is an c ");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Your grade is an d ");
+        }
+        else if (percent < 60)
+        {
+            Console.WriteLine("Your grade is an F ");
+        }
+        if (percent >= 70 )
+        {
+            Console.WriteLine(" CONGRATS you have passed the class");
+        }
+        else 
+        {
+            Console.WriteLine("Saddly you have not passed the test and will have to try again. Good Luck!");
+        }
+
     }
 }
