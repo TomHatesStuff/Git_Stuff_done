5d52691c40ab0c95b58220cd7155a7688bd98a29
TomHatesStuff <tlukemarshall1@gmail.com>
2023-09-22 17:53:57 -0600

    C sharp 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomMagic = new Random();
+        int MagicNumber = randomMagic.Next (1,101);
+
+        int Guess = -1;
+
+        while (MagicNumber != Guess )
+        {
+        Console.Write("what is you guess? ");
+        Guess = int.Parse(Console.ReadLine());
+
+        if (MagicNumber < Guess)
+        {
+            Console.WriteLine("Lower ");
+        }
+        else if (MagicNumber > Guess)
+        {
+            Console.WriteLine ("Higher ");
+        }
+        else
+        {
+            Console.WriteLine("Correct! ");
+        
+        }
+        }
+    
     }
 }
