7d9866b595ceb5a115f8196b9b5359134137808d
TomHatesStuff <tlukemarshall1@gmail.com>
2023-09-25 11:40:06 -0600

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomMessage();
+        string UserName = PromptUserName();
+        int UserNumber = PromptUserNumber ();
+        int UserSquare = SquareNumber (UserNumber);
+        DisplayResults (UserName, UserSquare);
     }
+    static void DisplayWelcomMessage ()
+    {
+        Console.WriteLine ("Welcome to the Function Assignment!");
+    }
+    static string PromptUserName ()
+    {
+        Console.WriteLine("Please type you username in ");
+        string Name = Console.ReadLine ();
+        
+        return Name;
+    }
+    static int PromptUserNumber ()
+    {
+        Console.WriteLine ("What is your favorite number?");
+        int Number = int.Parse (Console.ReadLine());
+
+        return Number;
+    }
+    static int SquareNumber (int Number)
+    {
+        int Square = Number * Number;
+        
+        return Square;
+    }
+    static void DisplayResults(string Name , int Square)
+    {
+        Console.WriteLine ($"{Name} the Square of your number is {Square} ");
+    }
+
 }
